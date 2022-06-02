590d53cdaa8394e06260b6884763d5cd461cc40b
Ruan Scherer <ruan.scherer@outlook.com>
2022-06-01 21:53:25 -0300

    feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
